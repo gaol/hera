0e8d3c65ab5e10a0567c6948f5efd5f83d38c6e8
Lin Gao <aoingl@gmail.com>
2022-05-05 16:55:57 +0800

    debug info

--- job.sh
+++ job.sh
@@ -34,7 +34,7 @@ env_file_if_enabled() {
 }
 
 set +u
-run_ssh "podman exec $(env_file_if_enabled) \
+run_ssh "pwd && ls -l && podman exec $(env_file_if_enabled) \
         -e LANG='en_US.utf8' \
         -e JOB_NAME="${JOB_NAME}" \
         -e PARENT_JOB_NAME="${PARENT_JOB_NAME}" \
